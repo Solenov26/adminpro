bf989a5328dd117f1e9785948a8015fd5425cf48
Carlos Enrique Amador Oviedo <carlosenrique.ce@gmail.com>
2018-09-06 21:51:34 -0500

    Modificaciones en los modulos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
+//Rutas
+import { PAGES_ROUTES } from './pages.routes';
+//Modulos
+import { SharedModule } from '../shared/shared.module';
+//Componentes
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
-    declarations: [],
-    imports:[],
-    providers:[],
-    exports:[],
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent
+  ],
+  imports: [
+      SharedModule,
+      PAGES_ROUTES
+  ],
+  exports: [
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent
+  ]
 })
-
-export class PagesModule{}
+export class PagesModule {}
